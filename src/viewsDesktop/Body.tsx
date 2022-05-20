5c7217946c8a11fc5894772dc032af49053df92f
Felix Wu <felix@2simple.com>
2022-05-20 19:13:40 +0100

    Intro animation

--- src/viewsDesktop/Body.tsx
+++ src/viewsDesktop/Body.tsx
@@ -1,31 +1,55 @@
 import styled from 'styled-components'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import consts from '../utils/consts'
 import { getTheme, getTrayPosition, store } from '../utils/store'
 import { FrontPanel } from './FrontPanel'
 import { TrayContent } from './TrayContent'
 
 export function Body() {
+    const [isSmall, setIsSmall] = useState(true)
+    const [isBlank, setIsBlank] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsSmall(false)
+            setTimeout(() => {
+                setIsBlank(false)
+            }, consts.transition)
+        })
+    }, [])
+
     return (
         <Tray style={trayStyle()}>
-            <FrontPanel />
+            <div style={frontPanelStyle()}>
+                <FrontPanel />
+            </div>
             <TrayContent />
         </Tray>
     )
 
     function trayStyle(): React.CSSProperties {
         const unitSpaces = Math.max(consts.minBodyWidth, Math.min(store.state.chords.length + 1, consts.maxBodyWidth))
+        const fullWidth = unitSpaces * (consts.buttonWidth + consts.margin) + consts.margin
+        const fullHeight = isBlank ? consts.panelHeight : consts.panelHeight + getTrayPosition()
         return {
-            width: `${unitSpaces * (consts.buttonWidth + consts.margin) + consts.margin}px`,
-            height: `${consts.panelHeight + getTrayPosition()}px`,
+            width: `${isSmall ? 50 : fullWidth}px`,
+            height: `${isSmall ? 50 : fullHeight}px`,
             backgroundColor: getTheme().trayColour,
             boxShadow: `0 0 ${consts.shadowBlur}px 0 ${getTheme().shadowColour}`,
+            borderRadius: `${isSmall ? 100 : consts.borderRadius}px`,
+            border: `solid ${isSmall ? getTheme().knobLineColour : 'transparent'} ${isSmall ? 2 : 0}px`,
+        }
+    }
+
+    function frontPanelStyle(): React.CSSProperties {
+        return {
+            opacity: isBlank ? 0 : 1,
+            transition: `${consts.transition}ms`,
         }
     }
 }
 
 const Tray = styled.div`
-    border-radius: ${consts.borderRadius}px;
     overflow: hidden;
     transition: ${consts.transition}ms;
 `
