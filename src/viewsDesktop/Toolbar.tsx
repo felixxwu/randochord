131dcfc600db58eede35b44f2535e6a058f6802e
Felix Wu <felix@2simple.com>
2022-06-18 12:40:35 +0100

    Add small logo in toolbar

--- src/viewsDesktop/Toolbar.tsx
+++ src/viewsDesktop/Toolbar.tsx
@@ -3,10 +3,15 @@ import consts from '../utils/consts'
 import { compute } from '../utils/store'
 import React from 'react'
 import { ToolbarButtons } from '../components/ToolbarButtons'
+import { Icon } from '../components/Icon'
+import logo from '../images/logo.svg'
 
 export function Toolbar() {
     return (
         <ToolbarDiv style={style()}>
+            <Logo>
+                <Icon src={logo} alt={'Logo'} size={'small'} />
+            </Logo>
             <Title>RandoChord</Title>
             <ToolbarButtons />
         </ToolbarDiv>
@@ -34,4 +39,12 @@ const Title = styled.div`
     flex: 1;
     text-transform: uppercase;
     letter-spacing: ${consts.titleSpacing}px;
+`
+
+const Logo = styled.div`
+    display: flex;
+    justify-content: center;
+    width: ${consts.smallButtonHeight}px;
+    transform: translateY(1px);
+    padding-right: 5px;
 `
